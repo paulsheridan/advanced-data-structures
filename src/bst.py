380fcccdc266cee7a735eebe973e5233d91803e6
paulsheridan <paul.sheridan@me.com>
2016-04-06 18:25:16 -0700

    still broken

--- src/bst.py
+++ src/bst.py
@@ -1,42 +1,57 @@
 class Node(object):
     def __init__(self, val):
         self.val = val
-        self.left = None
-        self.right = None
+        self._left = None
+        self._right = None
         self.parent = None
+
+        @property
+        def left(self):
+            return self._left
+
+        @left.setter
+        def left(self, left):
+            self._left = left
+
+        @property
+        def right(self):
+            return self._right
+
+        @right.setter
+        def right(self, right):
+            self._right = right
 
 
 class BinarySearchTree(object):
     def __init__(self, itbl=None):
         self.root = None
-        self.__size = 0
+        self._size = 0
         if itbl is not None:
             try:
                 for item in itbl:
                     self.insert(item)
-                    print(item)
             except TypeError:
                 print('That is not an iterable')
 
     def insert(self, val):
+        self._size += 1
         parent = None
         current = self.root
-        self.__size += 1
         new = Node(val)
-        current = new
-        print('root', self.root.val)
-        current.parent = parent
+        while current:
+            if new.val > current.val:
+                parent = current
+                current = current.right
+            elif new.val < current.val:
+                parent = current
+                current = current.left
+            else:
+                print('That value already exists in this tree')
+        if current == self.root:
+            self.root = new
+        else:
+            current = new
+            new.parent = parent
 
     def size(self):
-        return self.__size
-
-
-        # while current:
-        #     if new.val > current.val:
-        #         parent = current
-        #         current = current.right
-        #     elif new.val < current.val:
-        #         parent = current
-        #         current = current.left
-        #     else:
-        #         print('That value already exists in this tree')
+        return self._size
--- src/test_bst.py
+++ src/test_bst.py
@@ -3,54 +3,54 @@ import pytest
 
 # @pytest.mark.parametrize('')
 
-def test_make_empty_tree():
+@pytest.fixture(scope='function')
+def empty_tree():
     from bst import BinarySearchTree
-    test_tree = BinarySearchTree()
-    assert test_tree
+    empty = BinarySearchTree()
+    return empty
 
 
-def test_empty_tree_value():
+@pytest.fixture(scope='function')
+def test_tree():
     from bst import BinarySearchTree
-    test_tree = BinarySearchTree()
-    assert not test_tree.root
+    test = BinarySearchTree([5])
+    return test
 
 
-def test_make_one_node_tree():
-    from bst import BinarySearchTree
-    test_tree = BinarySearchTree([5])
+def test_make_empty_tree(empty_tree):
+    assert empty_tree
+
+
+def test_empty_tree_value(empty_tree):
+    assert not empty_tree.root
+
+
+def test_make_one_node_tree(test_tree):
     assert test_tree.root.val == 5
 
 
-def test_enter_first_node():
-    from bst import BinarySearchTree
-    test_tree = BinarySearchTree()
-    test_tree.insert(6)
-    assert test_tree.root.val == 6
+def test_enter_first_node(empty_tree):
+    empty_tree.insert(6)
+    assert empty_tree.root.val == 6
 
 
-def test_first_node_children():
-    from bst import BinarySearchTree
-    test_tree = BinarySearchTree([7])
+def test_first_node_children(test_tree):
+    # result = test_tree.root._left
+    # assert result is None
     assert test_tree.root.left is None
     assert test_tree.root.right is None
 
 
-def test_empty_tree_size():
-    from bst import BinarySearchTree
-    test_tree = BinarySearchTree()
-    assert test_tree.size() == 0
+def test_empty_tree_size(empty_tree):
+    assert empty_tree.size() == 0
 
 
-def test_first_entry_size():
-    from bst import BinarySearchTree
-    test_tree = BinarySearchTree()
-    test_tree.insert(8)
-    assert test_tree.size() == 1
+def test_first_entry_size(empty_tree):
+    empty_tree.insert(8)
+    assert empty_tree.size() == 1
 
 
-def test_insert_first_child():
-    from bst import BinarySearchTree
-    test_tree = BinarySearchTree([9])
+def test_insert_first_child(test_tree):
+    # import pdb; pdb.set_trace()
     test_tree.insert(10)
-    test_tree.insert(3)
     assert test_tree.root.right.val == 10
