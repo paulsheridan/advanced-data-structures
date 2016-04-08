3e1374b5219816eb07ba4c5cda542ce8650422c5
Rick Tesmond <tesmonrd@gmail.com>
2016-04-08 15:45:54 -0700

    working depth

--- src/bst.py
+++ src/bst.py
@@ -1,11 +1,20 @@
 class Tree(object):
+    """Single class implementation of BST."""
+
     def __init__(self, data):
-        self.left = None
-        self.right = None
-        self.data = data
-        self.__size = 0
+        """Initialize."""
+        try:
+            self.left = None
+            self.right = None
+            self.parent = None
+            self.data = data
+            self.depth_count = 1
+            self.__size = 0
+        except TypeError:
+            return("Please plant a root ex. 'Tree(10)'")
 
     def insert(self, data):
+        """Insert data into tree."""
         if self.data:
             if data < self.data:
                 if self.left is None:
@@ -24,9 +33,9 @@ class Tree(object):
         self.__size += 1
 
     def contains(self, data):
+        """Check tree for data."""
         try:
             if self.data:
-                # import pdb; pdb.set_trace()
                 if data < self.data:
                     if self.left.data and data == self.left.data:
                         print("True")
@@ -47,8 +56,21 @@ class Tree(object):
             return False
 
     def size(self):
+        """Return size of tree."""
         print(self.__size)
         return self.__size
+
+    def _depth_inc(self):
+        self.depth_count += 1
+        return self.depth_count
+
+    def depth(self):
+        """Return depth of the tree."""
+        if self is None:
+            return 0
+        else:
+            depth_size = max(Tree.depth(self.left), Tree.depth(self.right)) + 1
+            return depth_size
 
 
 bst = Tree(10)
@@ -56,22 +78,14 @@ bst.insert(5)
 bst.insert(200)
 bst.insert(45)
 bst.insert(1)
-bst.insert(22)
-bst.insert(25)
+bst.insert(4)
 bst.insert(2000)
-bst.insert(2)
 bst.insert(3)
 print("***********************")
-bst.contains(1)
-bst.contains(5)
-bst.contains(200)
-bst.contains(45)
-bst.contains(1)
-bst.contains(3)
-bst.contains(22)
-bst.contains(2)
-bst.contains(25)
-bst.contains(33333)
-bst.contains(2000)
+# bst.contains(1)
+# bst.contains(5)
+# bst.contains(200)
+# bst.contains(45)
 
 bst.size()
+print(bst.depth())
