8dbe931a2b48ea60097206e70f34b0bd4ae1471a
Rick Tesmond <tesmonrd@gmail.com>
2016-04-08 13:52:59 -0700

    implemented single class BST, contains, insert and size all working

--- src/bst.py
+++ src/bst.py
@@ -1,57 +1,77 @@
-class Node(object):
-    def __init__(self, val):
-        self.val = val
-        self._left = None
-        self._right = None
-        self.parent = None
+class Tree(object):
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+        self.__size = 0
 
-        @property
-        def left(self):
-            return self._left
-
-        @left.setter
-        def left(self, left):
-            self._left = left
-
-        @property
-        def right(self):
-            return self._right
-
-        @right.setter
-        def right(self, right):
-            self._right = right
-
-
-class BinarySearchTree(object):
-    def __init__(self, itbl=None):
-        self.root = None
-        self._size = 0
-        if itbl is not None:
-            try:
-                for item in itbl:
-                    self.insert(item)
-            except TypeError:
-                print('That is not an iterable')
-
-    def insert(self, val):
-        self._size += 1
-        parent = None
-        current = self.root
-        new = Node(val)
-        while current:
-            if new.val > current.val:
-                parent = current
-                current = current.right
-            elif new.val < current.val:
-                parent = current
-                current = current.left
-            else:
-                print('That value already exists in this tree')
-        if current == self.root:
-            self.root = new
+    def insert(self, data):
+        if self.data:
+            if data < self.data:
+                if self.left is None:
+                    self.left = Tree(data)
+                    print(self.left.data)
+                else:
+                    self.left.insert(data)
+            elif data > self.data:
+                if self.right is None:
+                    self.right = Tree(data)
+                    print(self.right.data)
+                else:
+                    self.right.insert(data)
         else:
-            current = new
-            new.parent = parent
+            self.data = data
+        self.__size += 1
+
+    def contains(self, data):
+        try:
+            if self.data:
+                # import pdb; pdb.set_trace()
+                if data < self.data:
+                    if self.left.data and data == self.left.data:
+                        print("True")
+                        return True
+                    else:
+                        self.left.contains(data)
+                elif data > self.data:
+                    if self.right.data and data == self.right.data:
+                        print("True")
+                        return True
+                    else:
+                        self.right.contains(data)
+                elif data == self.data:
+                    print("True")
+                    return True
+        except AttributeError:
+            print("False")
+            return False
 
     def size(self):
-        return self._size
+        print(self.__size)
+        return self.__size
+
+
+bst = Tree(10)
+bst.insert(5)
+bst.insert(200)
+bst.insert(45)
+bst.insert(1)
+bst.insert(22)
+bst.insert(25)
+bst.insert(2000)
+bst.insert(2)
+bst.insert(3)
+print("***********************")
+bst.contains(1)
+bst.contains(5)
+bst.contains(200)
+bst.contains(45)
+bst.contains(1)
+bst.contains(3)
+bst.contains(22)
+bst.contains(2)
+bst.contains(25)
+bst.contains(33333)
+bst.contains(2000)
+
+bst.size()
