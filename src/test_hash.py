a80ae6ea702e99020ee9cdddf38fd2488801277c
Rick Tesmond <tesmonrd@gmail.com>
2016-04-18 15:37:20 -0700

    all tests passing

--- src/test_hash.py
+++ src/test_hash.py
@@ -24,6 +24,10 @@ def test_empty_hash_exists(new_hash):
     assert new_hash
 
 
+def test_init(new_hash):
+    assert len(new_hash.table) == 11
+
+
 def test_non_default_hash_exists(large_hash):
     assert large_hash
 
@@ -38,3 +42,19 @@ def test_full_hashtable():
 
 
 def test_key_error(new_hash):
+    """Test key error."""
+    with pytest.raises(KeyError):
+        assert new_hash.set(123, "potato")
+        assert new_hash.set(cat, "dog")
+        assert new_hash.get(456, "seven, eight, nine")
+        assert new_hash.get(error, "so many")
+
+
+def test_hash(new_hash):
+    """Test hashing function."""
+    assert new_hash._hash("key") == 3
+
+
+def test_string_to_bits(new_hash):
+    """Test string to bits function."""
+    assert new_hash._str_to_bits("key") == 14
