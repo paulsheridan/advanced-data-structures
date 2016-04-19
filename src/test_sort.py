ea2ebb272e94b3edb4e29628ceef3e3171ffcd86
paulsheridan <paul.sheridan@me.com>
2016-04-18 19:10:39 -0700

    tox ini updated, one test for insertionsort added

--- src/test_sort.py
+++ src/test_sort.py
@@ -1,1 +1,19 @@
 import pytest
+import random
+
+
+@pytest.fixture(scope='session')
+def unsorted():
+    unsorted_list = [random.randint(0, 1000000) for i in range(1000)]
+    return unsorted_list
+
+
+def test_insertion_sort(unsorted):
+    from sort import insertionsort
+    sort_list = insertionsort(unsorted)
+    for ii in range(1, len(unsorted) - 1):
+        assert sort_list[ii] >= sort_list[ii - 1]
+
+
+def test_insertion_stable(unsorted):
+    pass
