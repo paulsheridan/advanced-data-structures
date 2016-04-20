a9d0b1e6aeba46c7fcedc3f95fd997a9c2b631dd
paulsheridan <paul.sheridan@me.com>
2016-04-20 15:07:29 -0700

    mergesort now working, still no tests or edge cases attempted

--- src/sort.py
+++ src/sort.py
@@ -13,27 +13,30 @@ def insertionsort(in_list):
 def mergesort(in_list):
     if len(in_list) > 1:
         mid = len(in_list)//2
-        left = in_list[:mid]
-        right = in_list[mid:]
-        mergesort(left)
-        mergesort(right)
+        left = mergesort(in_list[:mid])
+        right = mergesort(in_list[mid:])
         return(merge(left, right))
+    else:
+        return in_list
 
 
 def merge(left, right):
     new = []
-    while len(left) > 0 and len(right) > 0:
+    while left and right:
         if left[0] < right[0]:
             new.append(left.pop(0))
         elif right[0] < left[0]:
             new.append(right.pop(0))
         else:
-            new.extend(right.pop(0), left.pop(0))
-    if len(left) > 0:
+            new.append(left.pop(0))
+            new.append(right.pop(0))
+    if left:
         new.extend(left)
-    elif len(right) > 0:
+    elif right:
         new.extend(right)
     return new
+
+print(mergesort([99, 4, 6, 2, 7, 1, 4, 2, 13]))
 
 
 def wrapper(func, *args, **kwargs):
@@ -42,18 +45,18 @@ def wrapper(func, *args, **kwargs):
     return wrapped
 
 
-if __name__ == '__main__':
-    import timeit
-    LIST_ONE = [1, 2, 3, 4]
-    LIST_TWO = [random.randint(0, 1000000) for i in range(10000)]
-    print("Insertion sort")
-    print("Compares each item in the list to its neighbor.")
-    print("It's stable and most efficient on small lists.")
-    print("Input: [1, 2]")
-    wrapped = wrapper(insertionsort, LIST_ONE)
-    print('Runs: 500')
-    print('Average time: ', timeit.timeit(wrapped, number=500))
-    print("Input: [random.randint(0, 1000000) for i in range(10000)]")
-    print('Runs: 500')
-    wrapped = wrapper(insertionsort, LIST_TWO)
-    print('Average time: ', timeit.timeit(wrapped, number=500))
+# if __name__ == '__main__':
+#     import timeit
+#     LIST_ONE = [1, 2, 3, 4]
+#     LIST_TWO = [random.randint(0, 1000000) for i in range(10000)]
+#     print("Insertion sort")
+#     print("Compares each item in the list to its neighbor.")
+#     print("It's stable and most efficient on small lists.")
+#     print("Input: [1, 2]")
+#     wrapped = wrapper(insertionsort, LIST_ONE)
+#     print('Runs: 500')
+#     print('Average time: ', timeit.timeit(wrapped, number=500))
+#     print("Input: [random.randint(0, 1000000) for i in range(10000)]")
+#     print('Runs: 500')
+#     wrapped = wrapper(insertionsort, LIST_TWO)
+#     print('Average time: ', timeit.timeit(wrapped, number=500))
