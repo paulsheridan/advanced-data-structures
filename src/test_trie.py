a415dff1dc9eb1d5f2ac3afad0e7c5c50a70c495
paulsheridan <paul.sheridan@me.com>
2016-04-26 16:27:55 -0700

    more edge case tests for insert created

--- src/test_trie.py
+++ src/test_trie.py
@@ -1,5 +1,26 @@
 import pytest
 
+INSERT_CASES = [['', {}],
+              ['w', {'w': {'$': {}}}],
+              ['to', {'t': {'o': {'$': {}}}}],
+              ['ha ha', {'h': {'a': {'$': {}}}}],
+              ['it he', {'h': {'e': {'$': {}}}, 'i': {'t': {'$': {}}}}],
+              ['broke broken', {'b': {'r': {'o': {'k': {'e': {'$': {}, 'n': {'$': {}}}}}}}}],
+              ['a as asd asdf asdfg asdfgh asdfghj',
+               {'a': {'$': {},
+                      's': {'$': {},
+                            'd': {'$': {},
+                                  'f': {'$': {},
+                                        'g': {'$': {},
+                                              'h': {'$': {},
+                                                    'j': {'$': {}}}}}}}}}],
+              ["that's", {'t': {'h': {'a': {'t': {"'": {'s': {'$': {}}}}}}}}],
+              ["don't do done", {'d': {'o': {'$': {}, 'n': {"'": {'t': {'$': {}}}, 'e': {'$': {}}}}}}],
+              ['same same same same same same same same same same same same', {'s': {'a': {'m': {'e': {'$': {}}}}}}],
+              ['right right right right right rigged right right right', {'r': {'i': {'g': {'g': {'e': {'d': {'$': {}}}}, 'h': {'t': {'$': {}}}}}}}],
+              ['ba ab ba ab ba ab ba ab ba ab ba ab ba ab ba ab ba ab', {'a': {'b': {'$': {}}}, 'b': {'a': {'$': {}}}}],
+              ]
+
 
 @pytest.fixture(scope='function')
 def new_trie():
@@ -13,25 +34,12 @@ def test_creation(new_trie):
     assert new_trie.root == {}
 
 
-def test_insert_letter(new_trie):
-    """Test inser letter."""
-    new_trie.insert('w')
-    assert new_trie.root == {'w': {'$': {}}}
-
-
-def test_insert_word(new_trie):
-    """Test word insert."""
-    new_trie.insert('to')
-    assert new_trie.root == {'t': {'o': {'$': {}}}}
-
-
-def test_insert_same(new_trie):
-    """Test insert same token."""
-    new_trie.insert('ha ha')
-    assert new_trie.root == {'h': {'a': {'$': {}}}}
-
-
-def test_insert_space(new_trie):
-    """Test insert same token."""
-    new_trie.insert('it he')
-    assert new_trie.root == {'h': {'e': {'$': {}}}, 'i': {'t': {'$': {}}}}
+@pytest.mark.parametrize('case_in, case_out', INSERT_CASES)
+def test_insert_edge_cases(new_trie, case_in, case_out):
+    """
+    Test various insert cases including
+    empty string, single letter,
+    repeat words etc.
+    """
+    new_trie.insert(case_in)
+    assert new_trie.root == case_out
